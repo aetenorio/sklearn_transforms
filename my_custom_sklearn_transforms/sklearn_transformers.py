b3c0e6b9e94b4feb070c946dce900e24a3971462
aetenorio <aetenorio@gmail.com>
2020-08-21 22:47:41 -0500

    Add files via upload

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,6 @@
+import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
+
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -12,5 +14,32 @@ class DropColumns(BaseEstimator, TransformerMixin):
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
+
+        #We create a holder data without the columns
+        holder = data.drop(labels=self.columns, axis='columns')
+
+        #Pass the imputer
+        si = SimpleImputer(
+            missing_values=np.nan,  # los valores que faltan son del tipo ``np.nan`` (Pandas estándar)
+            #strategy='constant',  # la estrategia elegida es cambiar el valor faltante por una constante
+            #fill_value=0,  # la constante que se usará para completar los valores faltantes es un int64 = 0
+            #verbose=0,
+            copy=True,
+            strategy='mean'
+        )
+
+        si.fit(X=holder)
+
+        df_data_3 = pd.DataFrame.from_records(
+            data=si.transform(
+                X=holder
+            ),  
+            columns=self.columns
+        )
+
+
+        #Re add the profile col
+        df_data_3.insert(12,'PROFILE',df_data_1['PROFILE'], True)
+
         # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+        return df_data_3
